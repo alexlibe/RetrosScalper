2f5fe8bd90924a32cc324b6fda50d2e3803def21
Alex Libertowski <alexski1029@gmail.com>
2020-12-15 19:16:36 -0500

    Organized the project. Made classes static as it makes more sense

---
+++ src/RetrosScalper/StockBot.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+
+using RetrosScalper.Utilities;
+using RetrosScalper.Data;
+
+namespace RetrosScalper
+{
+    class StockBot
+    {
+        private List<Uri> urlsToScan;
+
+        public async Task Start()
+        {
+            if (!LoadUrls())
+            {
+                return;
+            }
+            
+            while (true)
+            {
+                Console.WriteLine("Getting page data...");
+                foreach (var url in urlsToScan)
+                {
+                    bool cardInStock = false;
+                    List<IItem> items;
+
+                    switch (url.Host)
+                    {
+                        case "www.bestbuy.com":
+                            using (var htmlResponse = await HttpHelper.GetResponse(url))
+                            {
+                                var html = await htmlResponse.Content.ReadAsStringAsync();
+                                items = await StockScanner.ScanBestBuy(html);
+                            }
+
+                            break;
+                        case "www.newegg.com":
+                            using (var htmlResponse = await HttpHelper.GetResponse(url))
+                            {
+                                var html = await htmlResponse.Content.ReadAsStringAsync();
+                                items = await StockScanner.ScanNewegg(html);
+                            }
+
+                            break;
+                        default:
+                            Console.WriteLine("Invalid URL");
+                            return;
+                    }
+
+                    foreach (var i in items)
+                    {
+                        if (i.InStock)
+                        {
+                            Console.WriteLine("Name: " + i.Name);
+                            Console.WriteLine("Is in stock: " + i.InStock.ToString());
+                            Console.WriteLine("URL: " + i.URL);
+                            Console.WriteLine("Price: " + (i.Price == null ? "N/A" : "$" + i.Price.ToString()) + "\n");
+
+                            cardInStock = true;
+                        }
+                    }
+
+                    if (cardInStock)
+                    {
+                        Console.Beep();
+                    }
+
+                    Thread.Sleep(250);
+                }
+
+                Console.WriteLine("Finished getting page data...\n");
+                Thread.Sleep(2000);
+            }
+        }
+
+        private bool LoadUrls()
+        {
+            const string URL_FILE = "Urls.txt";
+
+            // Creates the url file if it doesn't exist
+            if (!File.Exists(URL_FILE))
+            {
+                File.Create(URL_FILE).Dispose();
+                Console.WriteLine(URL_FILE + " has been created. Input all your urls to scan there line by line.");
+
+                return false;
+            }
+
+            // Reads through each line in the url file and scans them later
+            urlsToScan = new List<Uri>();
+            using (var sr = new StreamReader(URL_FILE))
+            {
+                string urlLine;
+                while ((urlLine = sr.ReadLine()) != null)
+                {
+                    urlsToScan.Add(new Uri(urlLine));
+                    Console.WriteLine(urlLine + '\n');
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/RetrosScalper/Utilities/StockScanner.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using RetrosScalper.Data;
+
+using AngleSharp;
+using AngleSharp.Html.Dom;
+
+namespace RetrosScalper.Utilities
+{
+    static class StockScanner
+    {
+        public static async Task<List<IItem>> ScanNewegg(string html)
+        {
+            var neweggItems = new List<IItem>();
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(req => req.Content(html));
+
+            var items = document.QuerySelectorAll("div.item-container"); // Gets the item list
+            foreach (var item in items)
+            {
+                IItem neweggItem = new NeweggItem();
+
+                var priceElementTree = item.QuerySelector("li.price-current"); // Gets the item price
+                if (priceElementTree.TextContent != string.Empty)
+                {
+                    string dollars = priceElementTree.QuerySelector("strong").TextContent; // Gets the dollar price
+                    string cents = priceElementTree.QuerySelector("sup").TextContent; // Gets the cent price
+
+                    neweggItem.Price = float.Parse(dollars + cents);
+                }
+
+                string outOfStockText = item.QuerySelector("p.item-promo")?.TextContent; // Checks if the "Out of stock" message is showing
+                if (outOfStockText == null)
+                {
+                    neweggItem.InStock = true;
+                }
+                else
+                {
+                    neweggItem.InStock = false;
+                }
+
+                var linkElement = (AngleSharp.Html.Dom.IHtmlAnchorElement)item.QuerySelector("a.item-title");
+                neweggItem.Name = linkElement.TextContent; // The name of the item
+                neweggItem.URL = linkElement.Href;
+                neweggItems.Add(neweggItem);
+            }
+
+            return neweggItems;
+        }
+
+        public static async Task<List<IItem>> ScanBestBuy(string html)
+        {
+            var bestBuyItems = new List<IItem>();
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(req => req.Content(html));
+
+            var items = document.QuerySelectorAll("li.sku-item"); // Gets the item list
+            foreach (var item in items)
+            {
+                IItem bestBuyItem = new BestBuyItem();
+
+                // Gets the link and name of the GPU thats been scanned
+                var gpuLinkElement = (IHtmlAnchorElement)item.QuerySelector("h4.sku-header").QuerySelector("a");
+                string gpuFullLink = "www.bestbuy.com" + gpuLinkElement.PathName;
+
+                // Gets the price of the GPU by searching for the element with a dollar sign and removing that dollar sign to parse it
+                var gpuPriceElement = item.QuerySelectorAll("span[aria-hidden='true']").Where(m => m.TextContent.Contains("$")).Single();
+                var gpuPrice = float.Parse(gpuPriceElement.TextContent.Substring(1, gpuPriceElement.TextContent.Length - 1));
+
+                // Checks if the GPU is in stock
+                var gpuInStockElement = item.QuerySelector("div.fulfillment-add-to-cart-button").QuerySelector("button");
+                if (gpuInStockElement.TextContent == "Add to Cart")
+                {
+                    bestBuyItem.InStock = true;
+                }
+                else
+                {
+                    bestBuyItem.InStock = false;
+                }
+
+                bestBuyItem.Name = gpuLinkElement.TextContent;
+                bestBuyItem.Price = gpuPrice;
+                bestBuyItem.URL = gpuFullLink;
+
+                bestBuyItems.Add(bestBuyItem);
+            }
+
+            return bestBuyItems;
+        }
+    }
+}
